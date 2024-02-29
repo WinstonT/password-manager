50c4c3fb36886e403da56ff4886bae50258f77a6
WinstonT <tandiw22@gmail.com>
2024-02-29 13:23:29 +0700

    add client side files

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div>
+      <main>
+        <div>Hello</div>
+      </main>
+    </div>
+  );
+}
---
+++ client/types/Entry.ts
@@ -0,0 +1,8 @@
+interface Entry {
+  id: number;
+  website: string;
+  username: string;
+  password: string;
+}
+
+export default Entry;
