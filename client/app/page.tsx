55fc28642ddc73db188bab1625905ba36402874b
WinstonT <tandiw22@gmail.com>
2024-03-02 14:27:52 +0700

    test client side env var

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,9 +2,7 @@ import Entry from "@/types/Entry";
 import EntriesListItem from "./components/EntriesListItem";
 
 async function getEntriesData(): Promise<Entry[]> {
-  const response = await fetch(
-    "https://password-manager-production-0987.up.railway.app/"
-  );
+  const response = await fetch(`${process.env.API_URL}`);
 
   const { entries } = await response.json();
   return entries;
