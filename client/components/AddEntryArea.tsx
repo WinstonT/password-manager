b03410d3afa1fa65e3765c232b0b257b3c77b6fc
WinstonT <tandiw22@gmail.com>
2024-03-03 17:23:31 +0700

    fix browser saving input data

--- client/components/AddEntryArea.tsx
+++ client/components/AddEntryArea.tsx
@@ -5,8 +5,9 @@ import AddEntryButton from "./AddEntryButton";
 
 import { handleSubmit } from "@/actions/actions";
 
-export default function AddEntryArea() {
+export default function AddEntryArea(this: any) {
   const ref = useRef<HTMLFormElement>(null);
+  ref.current?.reset();
 
   const [isAdding, setAdding] = useState(false);
 
@@ -30,6 +31,7 @@ export default function AddEntryArea() {
               await handleSubmit(formData);
             }}
             method="POST"
+            autoComplete="off"
           >
             <div className="flex flex-col">
               <label className="text-gray-500 text-xs">Website</label>
@@ -45,6 +47,8 @@ export default function AddEntryArea() {
                 type="text"
                 className="border border-gray-500 rounded-md"
                 name="Username"
+                autoComplete="off"
+                readOnly
               />
             </div>
             <div className="flex flex-col ml-4">
@@ -53,6 +57,8 @@ export default function AddEntryArea() {
                 type="password"
                 className="border border-gray-500 rounded-md"
                 name="Password"
+                autoComplete="off"
+                readOnly
               />
             </div>
             <div className="flex-1" />
